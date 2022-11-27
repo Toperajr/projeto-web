d15f82777913b16af9e78c7ee4730db60c19c8e7
Toperajr <66702776+Toperajr@users.noreply.github.com>
2022-11-27 00:34:20 -0400

    update

--- js/login/validations.js
+++ js/login/validations.js
@@ -1,3 +1,13 @@
 function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
 }
+function sair() {
+    var confirmado = confirm('Deseja sair?');
+    if(confirmado){
+        firebase.auth().signOut().then(() => {
+            window.location.href = "index.html";
+        }).catch(() => {
+            alert('Erro ao fazer logout');
+        })
+    }
+}
